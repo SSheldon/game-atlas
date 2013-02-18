7806c18b8671785a56d568999890dd45ac86420f
Steven Sheldon <steven@sasheldon.com>
2013-02-17 20:10:50 -0600

    Configured for Heroku.

--- game_atlas/settings.py
+++ game_atlas/settings.py
@@ -1,5 +1,7 @@
 # Django settings for game_atlas project.
 
+import dj_database_url
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -10,14 +12,8 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
+    # Parse database configuration from $DATABASE_URL
+    'default': dj_database_url.config()
 }
 
 # Local time zone for this installation. Choices can be found here:
