2660ce8891e94ea63fa4eccf89ed26430820a872
Steven Sheldon <steven@sasheldon.com>
2013-03-10 23:29:51 -0500

    Added Release select_all method.

--- games/models.py
+++ games/models.py
@@ -1,4 +1,12 @@
-from django.db import models
+from django.db import models, connection
+
+def dict_fetch_all(cursor):
+    """Return all rows from a cursor as a dict"""
+    desc = cursor.description
+    return [
+        dict(zip([col[0] for col in desc], row))
+        for row in cursor.fetchall()
+    ]
 
 class Genre(models.Model):
 
@@ -23,3 +31,18 @@ class Release(models.Model):
     class Meta:
         managed = False
         db_table = 'release'
+
+    @staticmethod
+    def select_all():
+        cursor = connection.cursor()
+
+        cursor.execute(
+            'SELECT game.title, release.release_date, '
+            'genre.name as "genre_name", platform.name as "platform_name" '
+            'FROM release '
+            'JOIN game ON release.game_id=game.id '
+            'JOIN platform ON release.platform_id=platform.id '
+            'JOIN genre ON game.genre_id=genre.id '
+        )
+
+        return dict_fetch_all(cursor)
