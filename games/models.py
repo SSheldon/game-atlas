6c05ea2d85807c964b2c12cd48b9a374b1119967
Steven Sheldon <steven@sasheldon.com>
2013-03-14 01:15:41 -0500

    Added game queries.

--- games/models.py
+++ games/models.py
@@ -1,4 +1,4 @@
-from django.db import models, connection
+from django.db import models, connection, transaction
 
 def dict_fetch_all(cursor):
     """Return all rows from a cursor as a dict"""
@@ -19,6 +19,53 @@ class Game(models.Model):
     class Meta:
         managed = False
         db_table = 'game'
+
+    @staticmethod
+    def select(game_id):
+        cursor = connection.cursor()
+
+        query = (
+            'SELECT game.title, genre.name as "genre_name" '
+            'FROM game JOIN genre ON game.genre_id=genre.id '
+            'WHERE game.id = %s'
+        )
+        cursor.execute(query, (game_id,))
+
+        row = cursor.fetchone()
+        if not row:
+            return {}
+        return dict(zip((col[0] for col in cursor.description), row))
+
+    @staticmethod
+    def insert(title, genre_id):
+        cursor = connection.cursor()
+
+        query = (
+            'INSERT INTO game (title, genre_id) VALUES (%s, %s) RETURNING id'
+        )
+        cursor.execute(query, (title, genre_id))
+        transaction.commit_unless_managed()
+
+        row = cursor.fetchone()
+        if not row:
+            return None
+        return row[0]
+
+    @staticmethod
+    def update(game_id, title, genre_id):
+        cursor = connection.cursor()
+
+        query = 'UPDATE game SET title=%s, genre_id=%s WHERE id = %s'
+        cursor.execute(query, (title, genre_id, game_id))
+        transaction.commit_unless_managed()
+
+    @staticmethod
+    def delete(game_id):
+        cursor = connection.cursor()
+
+        query = 'DELETE FROM game WHERE id = %s'
+        cursor.execute(query, (game_id,))
+        transaction.commit_unless_managed()
 
 class Platform(models.Model):
 
