97006f352c6f48258c225160194919bad4a4b964
Jonathan Albrecht <jonathan.albrecht1@gmail.com>
2013-04-11 19:54:11 -0500

    [friends] Fixed sql syntax error

--- friends/sql/friends.sql
+++ friends/sql/friends.sql
@@ -1,5 +1,5 @@
 CREATE TABLE friends (
     user_id INTEGER NOT NULL REFERENCES auth_user(id),
     friend_id INTEGER NOT NULL REFERENCES auth_user(id),
-    PRIMARY KEY (user_id1, user_id2),
+    PRIMARY KEY (user_id, friend_id)
 );
