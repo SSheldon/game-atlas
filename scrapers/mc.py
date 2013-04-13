b3a9bec32251a01ab588a237941420d53aa29477
Jonathan Albrecht <jonathan.albrecht1@gmail.com>
2013-04-13 18:02:15 -0500

    changed tabs to spaces

--- scrapers/mc.py
+++ scrapers/mc.py
@@ -3,11 +3,11 @@ import urllib
 import re
 
 def get_score(game):
-	url = urllib.urlopen("http://www.metacritic.com/game/xbox-360/" + game)
+    url = urllib.urlopen("http://www.metacritic.com/game/xbox-360/" + game)
 
-	soup = BeautifulSoup(url)
+    soup = BeautifulSoup(url)
 
-	for span in soup.findAll('span', {'class':'score_value', 'property':'v:average'}):
-		#We will need to trim this somehow, but it returns the right score
-		return span
+    for span in soup.findAll('span', {'class':'score_value', 'property':'v:average'}):
+        #We will need to trim this somehow, but it returns the right score
+        return span
 
