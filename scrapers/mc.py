0a6e61e62e38ac94f985cea95c97ba5f7fedc88e
Jonathan Albrecht <jonathan.albrecht1@gmail.com>
2013-04-13 17:51:22 -0500

    I didn't commit the whole file

---
+++ scrapers/mc.py
@@ -0,0 +1,17 @@
+from BeautifulSoup import BeautifulSoup
+import urllib
+import re
+
+def get_score(game):
+	url = urllib.urlopen("http://www.metacritic.com/game/xbox-360/" + game)
+
+	soup = BeautifulSoup(url)
+
+	for span in soup.findAll('span', {'class':'score_value', 'property':'v:average'}):
+		#We will need to trim this somehow, but it returns the right score
+		return span
+
+def main():
+	print get_score("bioshock")
+if __name__ == '__main__': 
+	main() 
