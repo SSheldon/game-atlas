e744e09d003201c4e6b7faaef6abb8499153db9e
Jonathan Albrecht <jonathan.albrecht1@gmail.com>
2013-04-13 21:41:50 -0500

    got rid of "main" in metacritic

--- scrapers/metacritic.py
+++ scrapers/metacritic.py
@@ -2,33 +2,28 @@ from bs4 import BeautifulSoup
 import urllib
 import re
 
-def get_score(game, soup):
+def get_score(soup):
     return soup.find('span', {'class':'score_value', 'property':'v:average'}).string
         #We will need to trim this somehow, but it returns the right score
 
-def get_genre(game, soup):
+def get_genre(soup):
     for span in soup.findAll('span', {'class': 'label'}):
         if span.string == 'Genre(s):':
             return span.findNext('span').string
 
-def get_release(game, soup):
+def get_release(soup):
     for span in soup.findAll('span', {'class': 'label'}):
         if span.string == 'Release Date:':
             return span.findNext('span').string
 
-def getGameInfo(game, platform):
+def get_info(game, platform):
     url = urllib.urlopen("http://www.metacritic.com/game/" + platform +"/" + game)
 
     soup = BeautifulSoup(url)
     info_dict = {}
 
-    info_dict['score'] = get_score(game, soup)
-    info_dict['genre'] =  get_genre(game, soup)
-    info_dict['release'] =  get_release(game, soup)
+    info_dict['score'] = get_score(soup)
+    info_dict['genre'] =  get_genre(soup)
+    info_dict['release'] =  get_release(soup)
 
     return info_dict
-def main():
-    test = getGameInfo('bioshock-infinite', 'playstation-3')
-    print test
-if __name__ == '__main__': 
-     main() 
