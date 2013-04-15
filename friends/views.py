53fc16d874d495195cf6b57aec3c0cc5681de7b7
Jonathan Albrecht <jonathan.albrecht1@gmail.com>
2013-04-14 19:12:27 -0500

    Fixed no reverse errors in urls.py

--- friends/views.py
+++ friends/views.py
@@ -10,6 +10,10 @@ def friend_request_send(request, username):
     pass
 
 @login_required
+def friend_info(request, friend_id):
+	return redirect('friends:show_friends')
+
+@login_required
 def friend_request_accept(request):
     pass
 
@@ -20,8 +24,8 @@ def friend_request_reject(request):
 @login_required
 def friend_remove(request, friend_id):
     Friends.remove_friends(request.user.id, friend_id)
-
-    return redirect('friends:list')
+    context = {'friends': Friends.get_friends(request.user.id)}
+    return render(request, 'friends_list.html', context)
 
 @login_required
 def show_friends(request):
