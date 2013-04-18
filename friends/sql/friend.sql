90ceaded2ef03f6721dea05b14469ced66940f47
Jonathan Albrecht <jonathan.albrecht1@gmail.com>
2013-04-18 13:07:07 -0500

    Got rid of type_id in friend.sql

--- friends/sql/friend.sql
+++ friends/sql/friend.sql
@@ -1,6 +1,5 @@
 CREATE TABLE friend (
     user_id INTEGER NOT NULL REFERENCES auth_user(id),
     friend_id INTEGER NOT NULL REFERENCES auth_user(id),
-    type_id INTEGER, 
     PRIMARY KEY (user_id, friend_id)
 );
