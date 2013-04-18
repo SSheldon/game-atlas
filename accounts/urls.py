de96a7dfc23e0bdae8f7a402da9a0ac51c80250c
Steven Sheldon <steven@sasheldon.com>
2013-04-18 15:35:08 -0500

    [accounts] Added namespace to accounts urls.

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import patterns, url
 from accounts import views
 
 urlpatterns = patterns('',
-    url(r'^login/$', 'django.contrib.auth.views.login'),
-    url(r'^logout/$', 'django.contrib.auth.views.logout'),
+    url(r'^login/$', 'django.contrib.auth.views.login', name='login'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', name='logout'),
     url(r'^profile/$', views.profile, name='profile'),
     url(r'^signup/$', views.signup, name='signup'),
 )
--- game_atlas/urls.py
+++ game_atlas/urls.py
@@ -11,7 +11,7 @@ urlpatterns = patterns('',
 
     url(r'^$', 'games.views.index', name='home'),
     url(r'^games/', include('games.urls', namespace='games')),
-    url(r'^accounts/', include('accounts.urls')),
+    url(r'^accounts/', include('accounts.urls', namespace='accounts')),
     url(r'^friends/', include('friends.urls', namespace='friends')),
     url(r'^users/', include('users.urls')),
 
