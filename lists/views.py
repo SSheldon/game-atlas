54538b1a5a744b56a1d8d6a517cb44304c8218a0
Steven Sheldon <steven@sasheldon.com>
2013-04-23 18:03:53 -0500

    [lists] Added games template.

--- lists/views.py
+++ lists/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 from lists.models import UserGame
 
 def user_games(request, user_id):
-    context = {'games_list': UserGame.get_games(user_id)}
-    return render(request, 'games/index.html', context)
+    context = {'games': UserGame.get_games(user_id)}
+    return render(request, 'lists/games.html', context)
