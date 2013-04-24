574ce83ab11cc59e739f3a39f1a4af103d9307df
Steven Sheldon <steven@sasheldon.com>
2013-04-23 19:27:58 -0500

    [lists] Added add_game view.

--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render
+from django.views.decorators.http import require_POST
 
 from lists.models import UserGame
 
 def user_games(request, user_id):
     context = {'games': UserGame.get_games(user_id)}
     return render(request, 'lists/games.html', context)
+
+@require_POST
+@login_required
+def add_game(request, next_page='accounts:games'):
+    UserGame.add_game(request.user.id, request.POST['game_id'])
+    return redirect(next_page)
