b74650282ac75fa9bbc42d69c92128ee8cbfddc8
Steven Sheldon <steven@sasheldon.com>
2013-04-25 00:27:45 -0500

    Added settings for static files.

--- game_atlas/settings.py
+++ game_atlas/settings.py
@@ -5,6 +5,8 @@ import dj_database_url
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 
+BASE_DIR = os.path.abspath(os.path.join(PROJECT_DIR, '..'))
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -55,7 +57,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -66,6 +68,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_DIR, 'static'),
 )
 
 # List of finder classes that know how to find static files in
@@ -114,6 +117,7 @@ INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
+    'django.contrib.staticfiles',
 )
 
 # A sample logging configuration. The only tangible logging
