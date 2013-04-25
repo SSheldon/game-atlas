bc57e5b7422f4004fb1a59a54662eae38f28937b
Steven Sheldon <steven@sasheldon.com>
2013-04-24 23:46:31 -0500

    Moved base template.

--- game_atlas/settings.py
+++ game_atlas/settings.py
@@ -1,6 +1,9 @@
 # Django settings for game_atlas project.
 
+import os.path
 import dj_database_url
+
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -99,6 +102,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_DIR, 'templates'),
 )
 
 INSTALLED_APPS = (
