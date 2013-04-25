8f4b7dcc7e4f202f4417b5eef80ae02d28c8730a
Andrew Roth <andrewroth1991@gmail.com>
2013-04-24 21:23:05 -0500

    recommendations now calculate the ranking of each pair of users, according the the user's index in the UserGames array

--- recommendations/collab_filter.py
+++ recommendations/collab_filter.py
@@ -1,3 +1,23 @@
+# Computes the dot product of two vectors
+def dot(VectorA, VectorB):
+	sum = 0
+	length = min(len(VectorA), len(VectorB))
+	for i in range(0, length):
+		sum += VectorA[i]*VectorB[i]
+	return sum
+
+# Computes the norm of a vector
+def norm(Vector, power):
+	radicand = 0
+	for element in Vector:
+		radicand += pow(element, power)
+	return pow(float(radicand), 1/float(power))
+
+# Gets the ranking of two users using vector space model search
+def getRanking(UserA, UserB):
+	return dot(UserA, UserB)/(norm(UserA, 2)*norm(UserB, 2))
+
+
 # Create matrix of Users x Games
 # Each row should be a User
 # Each column should be a Game
@@ -11,6 +31,7 @@ UserGames = [
 	]
 
 for currentUser in UserGames:
-	rankings = []
-	for otherUser in UserGames:
-		rankings.append(getRanking(currentUser, otherUser))
+	rankings = {}
+	for i, otherUser in enumerate(UserGames):
+		rankings[i] = getRanking(currentUser, otherUser)
+	print rankings
